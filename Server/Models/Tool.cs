6bf8508b677d948a641b32b406efd8915687ae52
Mohamad Yazan Musahher <mohamadym@uia.no>
2023-10-21 04:57:12 +0200

    Added logic to store tools at different Categories

---
+++ Server/Models/Tool.cs
@@ -0,0 +1,11 @@
+﻿namespace Server;
+
+public class Tool
+{
+    public int ToolId { get; set; }
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; }
+    public string Image { get; set; }  // Image URL or path
+    public int CategoryId { get; set; }  // Foreign key
+    public Category Category { get; set; }  // Navigation property
+}
---
+++ Server/Models/ToolRenting.cs
@@ -0,0 +1,17 @@
+﻿using Server.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Server;
+
+public class ToolRenting
+{
+
+    public int ToolRentingId { get; set; }
+    public string ApplicationUserId { get; set; }
+    public ApplicationUser ApplicationUser { get; set; }
+    public int ToolId { get; set; }
+    public Tool Tool { get; set; }
+    public DateTime RentingDate { get; set; }
+
+
+}
