7c8f1507d1263af8b7bbc8ed8d1f268903363d99
Mohamad Yazan Musahher <mohamadym@uia.no>
2023-10-21 21:57:44 +0200

    Edit style to view rent tools

--- client/src/component/Gallery/Gallery.jsx
+++ client/src/component/Gallery/Gallery.jsx
@@ -1,74 +1,63 @@
-﻿import React from 'react';
+﻿import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';  // Import useNavigate
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import './Gallery.css';
-import uiahjelp from "../assests/uiahjelp.jpg";
-import uialogo from '../assests/uialogo.png'
-
+import uialogo from '../assests/uialogo.png';
 
 function Gallery() {
+    const [tools, setTools] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const navigate = useNavigate();  // Instantiate useNavigate
+
+    useEffect(() => {
+        let url = 'http://localhost:5210/api/tools';
+        if (selectedCategory) {
+            url += `?categoryId=${selectedCategory}`;
+        }
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setTools(data))
+            .catch(error => console.error('Error:', error));
+    }, [selectedCategory]);
+
+    const handleToolClick = () => {
+        navigate('/login');  // Navigate to login page
+    };
+
     return (
         <div className="gallery-page">
             <div className="navbar">
                 <img src={uialogo} alt="Logo" className="gallery-navbar-logo" />
-
-                Navbar Content
+                
             </div>
             <div className="sidebar">
-                Sidebar Content
                 <div className="filter-section">
-                    <h3>Filtrer utstyr</h3>
-                    <div className="checkbox-item">
-                        <input type="checkbox" id="filter1" />
-                        <label htmlFor="filter1">Pc skjerm</label>
-                    </div>
-                    <div className="checkbox-item">
-                        <input type="checkbox" id="filter2" />
-                        <label htmlFor="filter2">Pc</label>
-                    </div>
-                    <div className="checkbox-item">
-                        <input type="checkbox" id="filter3" />
-                        <label htmlFor="filter2">Lader</label>
-                    </div>
-                    <div className="checkbox-item">
-                        <input type="checkbox" id="filter4" />
-                        <label htmlFor="filter2">Kamera</label>
-                    </div>
-                    <div className="checkbox-item">
-                        <input type="checkbox" id="filter5" />
-                        <label htmlFor="filter2">Hodetelefoner</label>
-                    </div>
-                    {/* ... Add more checkboxes as needed ... */}
+                    <h4>Filtrer utstyr</h4>
+                    <button className="btn btn-secondary mb-2" onClick={() => setSelectedCategory(null)}>All Categories</button>
+                    <button className="btn btn-secondary mb-2" onClick={() => setSelectedCategory(1)}>Monitor</button>
+                    <button className="btn btn-secondary mb-2" onClick={() => setSelectedCategory(2)}>Charger</button>
+                    <button className="btn btn-secondary mb-2" onClick={() => setSelectedCategory(3)}>Camera</button>
+                    <button className="btn btn-secondary mb-2" onClick={() => setSelectedCategory(4)}>Headphones</button>
+                    <button className="btn btn-secondary mb-2" onClick={() => setSelectedCategory(5)}>Ipad</button>
                 </div>
             </div>
-            <div className="image-container">
-                {/* Add your images here */}
-               <div className="image-group">
-                   <h2>Pc skjerm</h2>
-                <img src={uiahjelp} alt="Main" className="main-image"/>
-                <img src={uiahjelp} alt="Main" className="main-image"/>
-               </div>
-                <div className="image-group">
-                    <h2>Pc</h2>
-                    <img src={uiahjelp} alt="Main" className="main-image"/>
-                    <img src={uiahjelp} alt="Main" className="main-image"/>
+
+            <div className="container mt-5">
+                <div className="row">
+                    {tools.map(tool => (
+                        <div className="col-md-4 mb-4" key={tool.toolId} onClick={handleToolClick} style={{ cursor: 'pointer' }}>
+                            <div className="card">
+                                <img src={tool.image} alt={tool.name} className="card-img-top" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{tool.name}</h5>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                <div className="image-group">
-                    <h2>Lader</h2>
-                    <img src={uiahjelp} alt="Main" className="main-image"/>
-                    <img src={uiahjelp} alt="Main" className="main-image"/>
-                </div>
-                <div className="image-group">
-                    <h2>Kamera</h2>
-                    <img src={uiahjelp} alt="Main" className="main-image"/>
-                    <img src={uiahjelp} alt="Main" className="main-image"/>
-                </div>
-                <div className="image-group">
-                    <h2>Hodetelefoner</h2>
-                    <img src={uiahjelp} alt="Main" className="main-image"/>
-                    <img src={uiahjelp} alt="Main" className="main-image"/>
-                </div>
-                
-                {/* Add more images as needed */}
             </div>
+
             <div className="gallery-footer">
                 <p>© 2023 Your Company Name. All rights reserved.</p>
                 <ul>
@@ -77,10 +66,8 @@ function Gallery() {
                     <li><a href="#">Contact</a></li>
                 </ul>
             </div>
-
         </div>
-   
-);
+    );
 }
 
 export default Gallery;
