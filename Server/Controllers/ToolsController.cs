bae7bf8d2eecf6037cc72bedeba9db9c362dd0eb
Mohamad Yazan Musahher <mohamadym@uia.no>
2023-10-21 06:56:32 +0200

    Add Controll to handle Get req from db

---
+++ Server/Controllers/ToolsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToolsController : ControllerBase
+    {
+        private readonly ToolDbContext _context;
+
+        public ToolsController(ToolDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tool>>> GetTools([FromQuery] int? categoryId)
+        {
+            if (categoryId.HasValue)
+            {
+                return await _context.Tools
+                    .Where(t => t.CategoryId == categoryId.Value)
+                    .ToListAsync();
+            }
+            else
+            {
+                return await _context.Tools.ToListAsync();
+            }
+        }
+    }
+}
