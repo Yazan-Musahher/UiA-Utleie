5fbd53d181deda352d2af076f9ea065b16db3faa
Mohamad Yazan Musahher <mohamadym@uia.no>
2023-10-25 23:01:36 +0200

    fixing docker-compose

--- restore-db.sh
+++ restore-db.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-set -e
-pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" -v /docker-entrypoint-initdb.d/tool03.sql
+pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" -v /docker-entrypoint-initdb.d/tool03.dmp
