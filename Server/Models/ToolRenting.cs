f737192df079f090fa4b726f208a84e60d03f9cb
Mohamad Yazan Musahher <mohamadym@uia.no>
2023-11-03 12:12:36 +0100

    Extanded the model to able to rent the tools

--- Server/Models/ToolRenting.cs
+++ Server/Models/ToolRenting.cs
@@ -5,13 +5,10 @@ namespace Server;
 
 public class ToolRenting
 {
-
     public int ToolRentingId { get; set; }
-    public string ApplicationUserId { get; set; }
-    public ApplicationUser ApplicationUser { get; set; }
+    public string ApplicationUserId { get; set; } // Foreign key property
+    public virtual ApplicationUser ApplicationUser { get; set; } // Navigation property
     public int ToolId { get; set; }
-    public Tool Tool { get; set; }
+    public virtual Tool Tool { get; set; }
     public DateTime RentingDate { get; set; }
-
-
 }
