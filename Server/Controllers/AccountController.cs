565b1e27ac9e166cd03677c831fc8ba99da1f440
Mohamad Yazan Musahher <mohamadym@uia.no>
2023-11-04 18:34:14 +0100

    Edited login api to generate a token key for JWT

--- Server/Controllers/AccountController.cs
+++ Server/Controllers/AccountController.cs
@@ -1,7 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Server.Models;
 using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace Server.Controllers;
 
@@ -11,11 +15,13 @@ public class AccountController : ControllerBase
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IConfiguration _configuration;
 
-    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
     {
         _userManager = userManager;
         _signInManager = signInManager;
+        _configuration = configuration;
     }
 
     [HttpPost("signup")]
@@ -48,29 +54,48 @@ public class AccountController : ControllerBase
         return BadRequest(new { message = "User already exists", errors = ModelState });
     }
 
-   [HttpPost("login")]
-public async Task<IActionResult> Login([FromBody] Login model)
-{
-    if (ModelState.IsValid)
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] Login model)
     {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
         var user = await _userManager.FindByEmailAsync(model.Email);
-        if (user == null)
+        if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
         {
-            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-            return BadRequest(new { message = "Login failed", errors = ModelState });
-        }
+            // Generate the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id),
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Email, user.Email)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials =
+                    new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"]
+            };
 
-        var result = await _signInManager.PasswordSignInAsync(user, model.Password, isPersistent: true, lockoutOnFailure: false);
-        if (result.Succeeded)
-        {
-            return Ok(new { message = "Login successful", name = user.Name });
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { token = tokenString, name = user.Name, message = "Login successful" });
         }
-        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+        else
+        {
+            return BadRequest(new { message = "Login failed" });
+        }
     }
-    return BadRequest(new { message = "Login failed", errors = ModelState });
-}
 
-[HttpPost("logout")]
+
+    [HttpPost("logout")]
 public async Task<IActionResult> Logout()
 {
     await _signInManager.SignOutAsync(); // This will clear the cookie
