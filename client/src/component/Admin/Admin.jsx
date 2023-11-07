92381fefdd395cc630ab44aba1e38da81b4b6113
Isak Pettersen <isakp@uia.no>
2023-11-07 14:03:44 +0100

    Added Admin login page for the rigth branch

---
+++ client/src/component/Admin/Admin.jsx
@@ -0,0 +1,43 @@
+﻿
+import React, { useState } from 'react';
+import './Admin.css';
+
+function AdminLogin() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        // Handle login logic here
+        console.log('Login details:', { username, password });
+    };
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleLogin}>
+                <h2>Admin Login</h2>
+                <div className="input-group">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit" className="login-button">Log In</button>
+            </form>
+        </div>
+    );
+}
+
+export default AdminLogin;
