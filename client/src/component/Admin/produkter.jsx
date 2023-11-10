bf374fb9edd05a8ef2c0c44fbabf1476f989129e
Thomas Hetland <thomas.hetland@outlook.com>
2023-11-10 11:13:40 +0100

    navbar for admin

---
+++ client/src/component/Admin/produkter.jsx
@@ -0,0 +1,18 @@
+import NavBar from "./global/navbar";
+
+import React from "react";
+
+
+function AdminLogin() {
+  
+
+  return (
+    <div className='container'>
+      <NavBar/>
+    </div>
+  
+
+  );
+}
+
+export default AdminLogin;
